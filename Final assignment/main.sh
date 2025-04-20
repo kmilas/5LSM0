3164f3c88b4e841c06138bb7a5bf3a08a81f4d74
Konstantinos Milas <scur1350@int5.local.snellius.surf.nl>
2025-04-20 20:45:12 +0200

    Default model for training

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,13 +3,13 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 8 \
-    --epochs 200 \
-    --lr 0.00003 \
-    --crop_size 448 \
+    --epochs 100 \
+    --lr 0.00001 \
+    --crop_size 560 \
     --resume True \
-    --model-name eomt \
+    --model-name dinov2b_linear \
     --eval-freq 10 \
     --num-workers 10 \
     --seed 42 \
-    --clip-grad True \
-    --experiment-id "eomt-segm-adamW-warmup-polylr-3e-5-crop-448-batch-8-iters-60k-gradclip" \
+    --clip-grad False \
+    --experiment-id "dinov2b-linear-adamW--steplr-1e-5-crop-560-batch-8" \
